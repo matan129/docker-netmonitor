a132c028ceb69e5a7ad4277dc77a0761ebdc7acc
Matan Rosenberg <matan129@gmail.com>
2016-12-24 22:30:43 +0200

    Refactor sampler

--- sampler/sampler.py
+++ sampler/sampler.py
@@ -1,11 +1,10 @@
 import sys
 import socket
 import potsdb
-import urllib2
 import pyspeedtest
+import functools
 import logbook
 from tornado.ioloop import PeriodicCallback, IOLoop
-
 
 log = logbook.Logger('')
 log.handlers.append(logbook.StreamHandler(sys.stdout, bubble=True))
@@ -14,70 +13,48 @@ client = potsdb.Client('tsdb')
 st = pyspeedtest.SpeedTest()
 
 
-def internet(host='8.8.8.8', port=53, timeout=1000):
+def internet_connection(host='8.8.8.8', port=53, timeout=1000):
     s = None
     try:
         socket.setdefaulttimeout(timeout)
         s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         s.connect((host, port))
-        value = 1
-    except Exception as ex:    
-        log.exception()
-        value = 0
+        return 1
     finally:
         if s is not None:
             s.close()
+
+
+def generic_metric(metric_name, value_supplier):
+    value = 0
     
-    client.send('internet.connection', value)
-
-
-def download():
-    value = 0
-
     try:
-        value = st.download()
-    except Exception as ex:
-        log.exception()
+        value = value_supplier()
+    except KeyboardInterrupt:
+        log.critical('Interrupted while collecting {}, exiting...', metric_name)
+        sys.exit(2)
+    except:
+        log.exception('Error collecting {}:', metric_name)
 
-    client.send('internet.download', value)
-    log.info('internet.download: {}', value)
-
-
-def upload():
-    value = 0
-
-    try:
-        value = st.upload()
-    except Exception as ex:
-        log.exception()
-
-    client.send('internet.upload', value)
-    log.info('internet.upload: {}', value)
+    send_and_log(metric_name, value)
 
 
-def ping():
-    value = 0
-
-    try:
-        value = st.ping()
-    except Exception as ex:
-        log.exception()
-
-    client.send('internet.ping', value)
-    log.info('internet.ping: {}', value)
+def send_and_log(metric_name, value):
+    client.send(metric_name, value)
+    log.debug('{}: {}', metric_name, value)
 
 
-log.info('Test run...')
-internet()
-download()
-upload()
-ping()
+def main():    
+    log.info('Starting...')
+    log.debug('Registering callbacks')
+    PeriodicCallback(functools.partial(generic_metric, 'internet.connection', internet_connection), 500).start()
+    PeriodicCallback(functools.partial(generic_metric, 'internet.download', st.download), 120000).start()
+    PeriodicCallback(functools.partial(generic_metric, 'internet.upload', st.upload), 120000).start()
+    PeriodicCallback(functools.partial(generic_metric, 'internet.ping', st.ping), 120000).start()
 
-log.info('Registering callbacks')
-PeriodicCallback(internet, 1).start()
-PeriodicCallback(download, 120000).start()
-PeriodicCallback(upload, 120000).start()
-PeriodicCallback(ping, 120000).start()
+    log.info('Starting loop')
+    IOLoop.instance().start()
 
-log.info('Starting loop')
-IOLoop.instance().start()
+
+if __name__ == '__main__':        
+    main()
